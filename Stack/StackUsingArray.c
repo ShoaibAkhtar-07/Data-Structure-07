9c948590dcdc71395c2e44d49a9a10bf36d8f93f
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-10-30 16:26:19 +0530

    Create StackUsingArray.c

---
+++ Stack/StackUsingArray.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct stack
+{
+    int size;
+    int top;
+    int *arr;
+};
+
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int isFull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int main()
+{
+    struct stack *s;
+    s->size = 20;
+    s->top = -1;
+    s->arr = (int *)malloc(s->size * sizeof(int));
+
+    if(isEmpty(s)){
+        printf(" The stack is EMPTY\n");
+    }
+    else{
+        printf("The stack is not EMPTY");
+    }
+
+    return 0;
+}
