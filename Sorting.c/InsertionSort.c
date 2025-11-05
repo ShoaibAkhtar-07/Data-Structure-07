eee098f7c3290e9a6846d8fc0a567e77a1d73c96
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-11-05 21:24:21 +0530

    Update InsertionSort.c

--- Sorting.c/InsertionSort.c
+++ Sorting.c/InsertionSort.c
@@ -1,24 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void Display(int *Arr,int size){
+void Display(int *Arr, int size)
+{
     for (int i = 0; i < size; i++)
     {
-        printf("%d  ",Arr[i]);
+        printf("%d  ", Arr[i]);
     }
+    printf("\n");
 }
 
-void insertionsort(int *Arr,int size){
-    for (int i = 1; i < size - 1; i++)
+void insertionsort(int *Arr, int size)
+{
+    for (int i = 1; i < size; i++)
     {
-        
+        int temp = Arr[i];
+        int j = i - 1;
+        while (Arr[j] > temp && j >= 0)
+        {
+            Arr[j + 1] = Arr[j];
+            j--;
+        }
+        Arr[j + 1] = temp;
     }
-    
 }
 
 int main()
 {
-    int Arr[] = {2,13,19,12,7,98,65};
-    int size = sizeof(Arr)/sizeof(int);
-    Display(Arr,size);
+    int Arr[] = {2, 13, 19, 12, 7, 98, 65};
+    int size = sizeof(Arr) / sizeof(int);
+    Display(Arr, size);
+    insertionsort(Arr,size);
+    Display(Arr, size);
     return 0;
 }
