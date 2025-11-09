7aa2494f7d3de5a26a27f1931f077d6eea5c8a2d
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-11-09 18:10:23 +0530

    Update LinkedBinaryRepresentation.c

--- BinaryTree/LinkedBinaryRepresentation.c
+++ BinaryTree/LinkedBinaryRepresentation.c
@@ -27,17 +27,50 @@ void preOrder(struct Node *root)
     }
 }
 
+void postOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d  ", root->data);
+    }
+}
+
+void inOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("%d  ", root->data);
+        inOrder(root->right);
+    }
+}
+
 int main()
 {
-    struct Node *p = createNode(2);
-    struct Node *c1 = createNode(3);
-    struct Node *c2 = createNode(4);
+    struct Node *p = createNode(4);
+    struct Node *c1 = createNode(5);
+    struct Node *c2 = createNode(6);
     struct Node *gc1 = createNode(7);
     struct Node *gc2 = createNode(8);
+    struct Node *ggc1 = createNode(9);
+    struct Node *ggc2 = createNode(10);
+
     p->left = c1;
     p->right = c2;
     c1->left = gc1;
     c1->right = gc2;
+    gc1->left = ggc1;
+    gc1->right = ggc2;
+    printf("---PREOrder---\n");
     preOrder(p);
+    printf("\n");
+    printf("---POSTOrder---\n");
+    postOrder(p);
+    printf("\n");
+    printf("---INOrder---\n");
+    inOrder(p);
+
     return 0;
 }
